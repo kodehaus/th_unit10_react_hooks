f3e98fd9b99646f98fca0d50c2d8ab74314316b1
Ken Robinson <krobinson@kodehaus.com>
2021-03-31 01:54:36 -0400

    update initial state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function App () {
+  const [ score, setScore ] = useState(0); // [0, f]
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome!</h1>
+        <h1>Welcome! { score }</h1>
       </header>
+        <button onClick={() => setScore(score + 1 )}>Click me</button>
     </div>
   );
 }
