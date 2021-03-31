0197f16eb2e7f1644a39e4440fd6d0d3062a5282
Ken Robinson <krobinson@kodehaus.com>
2021-03-31 02:21:53 -0400

    fetch with useEffects

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
+const url = 'https://dog.ceo/api/breeds/image/random';
 
 function App () {
   const [ score, setScore ] = useState(0); // [0, f]
   const [message] = useState('Welcome');
+  const [data, setData] = useState('');
   useEffect(() => {
-    document.title = `${message}. Your score is ${score}`;
+    fetch(url)
+      .then(res => res.json())
+      .then(data => setData(data.message))
+      .catch(err => console.log('Oh noes!' + {score}, err))
   },[score]);
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Welcome! { score }</h1>
-      </header>
-        <button onClick={() => setScore(prevScore => prevScore + 1 )}>Click me</button>
+      <h1>The score is: {score}</h1>
+      <img src={data} alt="A random dog breed" /><br/>
+      <button onClick={() => setScore(prevScore => prevScore + 25 )}>Click me</button>
     </div>
   );
 }
